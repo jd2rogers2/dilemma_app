affd47b0e3c768d2dda3afac00934d034269da83
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-16 16:38:25 -0400

    new factor view and controller, create factor controller, all done

--- app/controllers/factors_controller.rb
+++ app/controllers/factors_controller.rb
@@ -6,13 +6,19 @@ class FactorsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @factor = Factor.create(factor_params)
+    redirect_to option_path(@factor.option_id)
   end
 
   def destroy
-    @factor = Factor.find_by(id: params[:format])
+    @factor = Factor.find_by(id: params[:id])
     @option = @factor.option
     @factor.delete
     redirect_to option_path(@option)
+  end
+
+  private
+  def factor_params
+    params.require(:factor).permit(:name, :points, :option_id)
   end
 end
