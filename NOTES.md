037f0c302200d411d20aed6ca05ef1d28bdf1753
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-19 17:36:56 -0400

    tag new and edit views and controllers, views abstracted to partial

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,7 @@
 <%# add option in below td for add tags link ??? %> from dilemmas index
 
 make dilemmas index table look nice
+
+<%# make this into links for each dilemma??? %>
+from tags index
+
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,21 @@
 class TagsController < ApplicationController
   def new
+    @tag = Tag.new
   end
 
   def create
+    @tag = Tag.create(tag_params)
+    redirect_to tag_path(@tag)
   end
 
   def edit
+    @tag = Tag.find_by(id: params[:id])
   end
 
   def update
+    @tag = Tag.find_by(id: params[:id])
+    @tag.update(tag_params)
+    redirect_to tag_path(@tag)
   end
 
   def show
@@ -20,5 +27,13 @@ class TagsController < ApplicationController
   end
 
   def destroy
+    @tag = Tag.find_by(id: params[:id])
+    @tag.destroy
+    redirect_to tags_path
+  end
+
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
