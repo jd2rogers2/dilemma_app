b3161598c131726df1d894019037c75deccca7df
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-20 11:39:34 -0400

    just changed routes file to resourced routes, check last commit to retract, all routes in views are still good here though

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get '/dilemmas/overdue', to: 'dilemmas#overdue', as: 'overdue'
-  resources :factors, only: [:destroy, :new, :create]
+  # resources :factors, only: [:destroy, :new, :create]
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   resources :dilemmas
   resources :tags
-  resources :options
+  # resources :options
   root to: 'welcome#home'
+
+  resources :dilemmas, only: [:show] do
+    resources :options do
+      resources :factors, only: [:destroy, :new, :create]
+    end
+  end
 end
