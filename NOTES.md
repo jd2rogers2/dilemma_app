c48b0c8fefa2eb1d229887481045dbdf37c54db5
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-22 16:02:16 -0400

    fixed minor bugs, dilemma_tags partial, comments in dilemma show

--- NOTES.md
+++ NOTES.md
@@ -4,3 +4,5 @@ notes from refactoring video
 -content_tag_for
 -can remove partial: and locals: from render
 -can yield back to partial from helper
+
+when creating new dilemma, new tag is created but didn't show up in show view
--- app/helpers/dilemmas_helper.rb
+++ app/helpers/dilemmas_helper.rb
@@ -1,10 +1,4 @@
 module DilemmasHelper
-  def dilemma_tags(dilemma)
-    dilemma.tags.each do |t|
-      return link_to(t.name, tag_path(t))
-    end
-  end
-
   def dilemma_link(dilemma)
     link_to(dilemma.name, dilemma_path(dilemma))
   end
