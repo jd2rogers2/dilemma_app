d4cd4a91b30ba9034e09ca8bf442db3e991caf7e
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-22 11:08:54 -0400

    created dilemma_header partials with dilemma_link and author_deadline helpers and used in many views

--- app/helpers/dilemmas_helper.rb
+++ app/helpers/dilemmas_helper.rb
@@ -4,4 +4,13 @@ module DilemmasHelper
       return link_to(t.name, tag_path(t))
     end
   end
+
+  def dilemma_link(dilemma)
+    link_to(dilemma.name, dilemma_path(dilemma))
+  end
+
+  def author_deadline(dilemma)
+    "author: #{dilemma.author.email} | deadline: #{dilemma.pretty_deadline}"
+  end
+
 end
